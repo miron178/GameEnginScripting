67a0566fbaca3abe69a3f79113804d38f387963d
[     ] <miron.bury@gmail.com>
2019-09-25 13:16:42 +0100

    Text entry

---
+++ TextEntry/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField]
+    private Text gameText;
+    [SerializeField]
+    private InputField inputText;
+    public void ButtonClick()
+    {
+        if (inputText.text == "")
+        {
+            gameText.text = "Hello World!";
+        }
+        else
+        {
+            gameText.text = "Hello " + inputText.text;
+        }
+          
+    }
+}
