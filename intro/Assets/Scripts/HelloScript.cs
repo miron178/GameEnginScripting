172e6427503638093082f362460d47fc488916ac
[     ] <miron.bury@gmail.com>
2019-09-25 11:59:14 +0100

    even more changes

--- intro/Assets/Scripts/HelloScript.cs
+++ intro/Assets/Scripts/HelloScript.cs
@@ -5,16 +5,27 @@ using UnityEngine.UI;
 
 public class HelloScript : MonoBehaviour
 {
+    int Counter = 0;
+
+    string originalText = "Counting Down: ";
+    string continuedText = "";
+
+    float floatDemo = 10.0f / 3.0f;
+    double doubleDemo = 10.0 / 3.0;
+
     // Start is called before the first frame update
     void Start()
     {
-        ChangeText = "Hello World!";
+        continuedText = "Floate value: " + floatDemo + "\n" + "Double value: " + doubleDemo;
+
+        ChangeText(originalText + "\n" + continuedText);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Counter++;
+        ChangeText(originalText + Counter + "\n" + continuedText);
     }
 
     private void ChangeText(string text)
