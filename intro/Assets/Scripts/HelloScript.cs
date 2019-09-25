3040d4f80ad14ed48403c48bc03ad5c237981929
[     ] <miron.bury@gmail.com>
2019-09-25 11:05:02 +0100

    GameEnginScripting Week1

---
+++ intro/Assets/Scripts/HelloScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeText = "Hello World!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeText(string text)
+    {
+        GetComponent<Text>().text = text;
+    }
+}
